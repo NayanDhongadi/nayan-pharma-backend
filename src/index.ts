52b01dfc087b050da3ac759d773bb9d168b47999
Nayan <nayandhongadi26@gmail.com>
2024-07-30 16:48:06 +0530

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 const express = require('express')
 // const userRoutes =require( './routes/routes')
 const userRoutes = require('./routes/routes')
+require('dotenv').config(); 
 
 
 const app = express();
-const port = 4000;
+const port = process.env.BACKEND_PORT || 4000;
 
 app.use(express.json());
 // app.use('/api', userRoutes);
