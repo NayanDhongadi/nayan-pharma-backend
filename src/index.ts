f4f2b8c603d5afef55707301689bfc2f5bf9dde5
Nayan <nayandhongadi26@gmail.com>
2024-07-30 15:08:28 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-<<<<<<< HEAD
 app.use('/api', userRoutes);
-=======
-app.use('/', userRoutes);
->>>>>>> origin/main
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
