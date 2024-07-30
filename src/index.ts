57a97b848804168461c67af0058a41da9a283f6c
Nayan Jayantrao Dhongadi <99018458+NayanDhongadi@users.noreply.github.com>
2024-07-30 14:51:53 +0530

    Initaial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const express = require('express')
+const userRoutes =require( './routes/routes')
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use('/api', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
