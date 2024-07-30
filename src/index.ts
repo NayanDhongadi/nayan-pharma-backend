678a4521f20639267560a48778d5016a4b1986e1
Nayan <nayandhongadi26@gmail.com>
2024-07-30 16:45:37 +0530

    add some name route

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,12 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use('/api', userRoutes);
+// app.use('/api', userRoutes);
+
+
+app.get('/some', (req:any, res:any) => {
+  res.send('Hello from some route');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
