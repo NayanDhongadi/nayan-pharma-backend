99167d27d78778330d4b517c665ed40ab3b6920f
Nayan <nayandhongadi26@gmail.com>
2024-07-30 17:10:15 +0530

    add any

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-const express = require('express')
-// const userRoutes =require( './routes/routes')
-const userRoutes = require('./routes/routes')
-require('dotenv').config(); 
-
-
+const express = require('express');
+const userRoutes = require('./routes/routes');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.BACKEND_PORT || 4000;
@@ -11,9 +8,14 @@ const port = process.env.BACKEND_PORT || 4000;
 app.use(express.json());
 app.use('/api', userRoutes);
 
-
 app.get('/some', (req:any, res:any) => {
   res.send('Hello from some route');
+});
+
+// Basic error handling middleware
+app.use((err:any, req:any, res:any, next:any) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 app.listen(port, () => {
