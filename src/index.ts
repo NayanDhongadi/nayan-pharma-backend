f0224eca87ce2f2e3d11cbb0e75aa09f08a09f2d
Nayan Jayantrao Dhongadi <99018458+NayanDhongadi@users.noreply.github.com>
2024-07-30 14:55:04 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 4000;
 
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use('/', userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
